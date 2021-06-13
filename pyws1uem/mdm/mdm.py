196236ef1445c2a88d812c6ad314b32cf8577b46
Marco Fuchs <marco.fuchs@live.de>
2021-06-13 16:04:57 +0200

    Edit API module descriptions
    Edited the class description of MAM, MEM, MDM and System

--- pyws1uem/mdm/mdm.py
+++ pyws1uem/mdm/mdm.py
@@ -1,6 +1,10 @@
 class MDM(object):
     """
     Base MDM class
+
+    Workspace ONE UEM REST APIs allows you to manage all the functionalities of Mobile Device Management (MDM).
+    The functionalities that are included but not limited to are device commands,
+    retrieval of compliance, profile, network, location, and event log details. 
     """
     def __init__(self, client):
         self.client = client
