be709d2c98d2f679dc9849a8fe598339a2a260ef
Marco Fuchs <marco.fuchs@live.de>
2021-06-17 08:04:53 +0200

    Initialize base class Tags

---
+++ pyws1uem/mdm/tags.py
@@ -0,0 +1,14 @@
+from .mdm import MDM
+
+class Tags(MDM):
+    """
+    Base MDM class
+
+    Workspace ONE UEM REST APIs allows you to manage all the functionalities of Mobile Device Management (MDM).
+    The functionalities that are included but not limited to are device commands,
+    retrieval of compliance, profile, network, location, and event log details. 
+    """
+    def __init__(self, client):
+        self.client = client
+
+# TODO: Implement the tagging resources ...
