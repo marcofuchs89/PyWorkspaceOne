9421bf99940bdee441387618d523d4a9f7d14a9b
Marco Fuchs <marco.fuchs@live.de>
2021-08-16 14:06:38 +0200

    Initialize tags.py

--- pyws1uem/mdm/tags.py
+++ pyws1uem/mdm/tags.py
@@ -4,9 +4,10 @@ class Tags(MDM):
     """
     Base MDM class
 
-    Workspace ONE UEM REST APIs allows you to manage all the functionalities of Mobile Device Management (MDM).
+    Workspace ONE UEM REST APIs allows you to manage
+    all the functionalities of Mobile Device Management (MDM).
     The functionalities that are included but not limited to are device commands,
-    retrieval of compliance, profile, network, location, and event log details. 
+    retrieval of compliance, profile, network, location, and event log details.
     """
     def __init__(self, client):
         self.client = client
