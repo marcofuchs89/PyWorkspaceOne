102ffe120d9b454360c947a2d5dfccadb9b5cda5
Marco Fuchs <marco.fuchs@live.de>
2021-08-18 17:30:06 +0200

    Code format and cleanup

--- pyws1uem/mdm/tags.py
+++ pyws1uem/mdm/tags.py
@@ -1,5 +1,6 @@
 from .mdm import MDM
 
+
 class Tags(MDM):
     """
     Base Tags Class
@@ -7,6 +8,7 @@ class Tags(MDM):
     Contains REST-API Calls to add Devices to specific tags to trigger dependent actions,
     such as installing profiles, apps or trigger compliance actions.
     """
+
     def __init__(self, client):
         MDM.__init__(self, client)
 
@@ -18,15 +20,14 @@ class Tags(MDM):
         Args:
             kwargs ([type]): [description]
         """
-        # API Call to follow
         path = f'/tags/{tag_id}/adddevices'
         device_to_add = {
             "BulkValues": {
                 "Value": [
                     device_id
-                    ]
-                    }
-                    }
+                ]
+            }
+        }
         response = MDM._post(self, path=path, json=device_to_add)
         return response
 
@@ -36,17 +37,13 @@ class Tags(MDM):
         Args:
             kwargs ([type]): [description]
         """
-        # API Call to follow
         path = f'/tags/{tag_id}/removedevices'
         device_to_add = {
             "BulkValues": {
                 "Value": [
                     device_id
-                    ]
-                    }
-                    }
+                ]
+            }
+        }
         response = MDM._post(self, path=path, json=device_to_add)
-        #if response['FailedItems'] >= 1:
-        #    return response
-        #else:
         return response
